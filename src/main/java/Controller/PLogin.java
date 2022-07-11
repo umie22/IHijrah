1852cec0df6b6ec55727f5d88336cfa28dce5e47
frhna <frhna@192.168.0.112>
2022-07-11 22:56:07 +0800

    part

--- src/main/java/Controller/PLogin.java
+++ src/main/java/Controller/PLogin.java
@@ -15,7 +15,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-@WebServlet("/PLogin")
+@WebServlet("/Login")
 public class PLogin extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
--- src/main/webapp/LoginParticipantBARU.jsp
+++ src/main/webapp/LoginParticipantBARU.jsp
@@ -101,7 +101,7 @@ span.psw {
   </div>
 </div>
 
-<form action="PLogin" method="post">
+<form action="Login" method="post">
 	<div class="item1">
  <h2 style="text-align:center;">Login</h2>
   <br>
