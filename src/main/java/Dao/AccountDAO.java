d10558b8abda3e44562f6e4337e04e790ccd4cbe
frhna <frhna@192.168.0.112>
2022-07-12 06:21:07 +0800

    accdao

--- src/main/java/Dao/AccountDAO.java
+++ src/main/java/Dao/AccountDAO.java
@@ -14,7 +14,7 @@ public class AccountDAO {
 	String dbURL = "jdbc:postgresql://ec2-176-34-215-248.eu-west-1.compute.amazonaws.com/delu1t92658u0";
     String user = "zaiaryvqbpwwcb";
     String pass = "731fafeb016f84ea7f87300cbd19a24ba3e96adbaaf92504bc8d945d0302489b";
-
+    //accdao
     protected Connection getConnection() {
         Connection con = null;
         try {
