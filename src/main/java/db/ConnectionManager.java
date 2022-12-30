71113cdfb1e2443887c6430bc609cfa982f2928e
MUHAMMAD RAZIQ <2020853322@student.uitm.edu.my>
2022-12-31 00:56:09 +0800

    Update Db

--- src/main/java/db/ConnectionManager.java
+++ src/main/java/db/ConnectionManager.java
@@ -8,9 +8,9 @@ public class ConnectionManager {
 	static Connection con;
 
 	private static final String DB_DRIVER = "org.postgresql.Driver";
-	private static final String DB_CONNECTION = "jdbc:postgres://ihijrah_user:rYEtlKrdeWnvR3vk55hoaNi9TVUqSusS@dpg-cee3pmta4997f3l19ho0-a.oregon-postgres.render.com/ihijrah";
-	private static final String DB_USER = "ihijrah_user";	
-	private static final String DB_PASSWORD = "rYEtlKrdeWnvR3vk55hoaNi9TVUqSusS";
+	private static final String DB_CONNECTION = "jdbc:postgresql://${{ PGUSER }}:${{ PGPASSWORD }}@${{ PGHOST }}:${{ PGPORT }}/${{ PGDATABASE }}";
+	private static final String DB_USER = "postgres";	
+	private static final String DB_PASSWORD = "gLGI0WLy7urtd6VreOky";
 
 	public static Connection getConnection() {
 		try {
